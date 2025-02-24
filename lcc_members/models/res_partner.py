7407b4bb1ab12b4f24b5b0481a4f4f44cbaa8bfc
Guillaume Roche <groche@guigeek.org>
2025-02-24 21:15:36 +0000

    Add SEPA checkbox

--- lcc_members/models/res_partner.py
+++ lcc_members/models/res_partner.py
@@ -135,6 +135,10 @@ class res_partner(models.Model):
         string=_("Accepte Newsletter"), required=False, translate=False, readonly=False
     )
 
+    prvlt_sepa = fields.Boolean(
+        string=_("Prélèvement SEPA"), required=False, translate=False, readonly=False
+    )
+
     changeeuros = fields.Text(
         string=_("Change euros mensuel"), required=False, translate=False, readonly=False
     )
